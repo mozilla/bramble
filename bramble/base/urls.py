1d535fef87dc193fabc1333fb73c127fd1c371f3
Peter Bengtsson <mail@peterbe.com>
2012-03-28 10:05:50 -0700

    starting on the machinecounts

--- bramble/base/urls.py
+++ bramble/base/urls.py
@@ -5,7 +5,7 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='bramble.base.index'),
-    url(r'^redis/$', views.redis_info, name='bramble.base.redis_info'),
+    url(r'^machinecounts/$', views.machinecounts, name='bramble.base.machinecounts'),
     url(r'^builds/$', api.get_builds, name='bramble.base.get_builds'),
     url(r'^builds/(?P<uid>\w{32})/jobs$', api.get_build_jobs,
         name='bramble.base.get_build_jobs'),
--- bramble/base/views.py
+++ bramble/base/views.py
@@ -1,31 +1,52 @@
+import datetime
+import functools
+import anyjson as json
+from django import http
 from django.conf import settings as django_settings
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 
 from redis_utils import redis_client
 
+
+def json_view(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kw):
+        response = f(*args, **kw)
+        if isinstance(response, http.HttpResponse):
+            return response
+        else:
+            return http.HttpResponse(json.dumps(response),
+                                     content_type='application/json')
+    return wrapper
+
+
 def index(request):
     data = {}
     return render(request, 'base/index.html', data)
 
-def redis_info(request):
-    """Admin view that displays redis INFO+CONFIG output for all backends."""
-    redis_info = {}
-    for key in django_settings.REDIS_BACKENDS.keys():
-        redis_info[key] = {}
-        client = redis_client(key)
-        redis_info[key]['connection'] = django_settings.REDIS_BACKENDS[key]
-        try:
-            cfg = client.config_get()
-            redis_info[key]['config'] = [{'key': k, 'value': cfg[k]} for k in
-                                         sorted(cfg)]
-            info = client.info()
-            redis_info[key]['info'] = [{'key': k, 'value': info[k]} for k in
-                                       sorted(info)]
-        except ConnectionError:
-            redis_info[key]['down'] = True
 
-    return render_to_response('base/redis.html',
-                              {'redis_info': redis_info,
-                               'title': 'Redis Information'},
-                              RequestContext(request, {}))
+
+@json_view
+def machinecounts(request):
+    data = {}
+    no_bars = int(request.GET.get('bars', 21))
+    resolution = int(request.GET.get('resolution', 60 * 60))
+
+    last = datetime.datetime.utcnow()
+    from random import randint
+    dates = []
+    for i in range(no_bars + 1):
+        _next = last - datetime.timedelta(seconds=resolution)
+        range_ = _next, last
+
+        #....
+        dates.append({
+          'date': range_[0].isoformat(),
+          'working': randint(10, 20),
+          'idle': randint(2, 8),
+          'error': randint(0, 5),
+        })
+        last = _next
+
+    return {'dates': dates}
